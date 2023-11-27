f596da4ff4d303a55bc3e537d18f2bcf6cf9246c
Stefan Grönberg <stefan@gronberg.info>
2023-11-27 23:41:19 +0100

    .

---
+++ Lootbox/Classes/enums.cs
@@ -0,0 +1,6 @@
+﻿namespace LootBox.classes;
+
+public enum Enums
+{
+    
+}
--- Lootbox/MainWindow.xaml.cs
+++ Lootbox/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Windows;
+using LootBox.classes;
 
-namespace Lootbox
+namespace LootBox
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -10,8 +11,21 @@ namespace Lootbox
         public MainWindow()
         {
             InitializeComponent();
-            
-            
+            Ss();
+
+        }
+
+        private void Ss()
+        {
+           var currentEventIdentifier = "LBI00069420";  
+            for (int index = 0; index < 5; index++)  
+            {  
+                currentEventIdentifier = EventIncrementer.Instance.EventSequence(currentEventIdentifier);  
+                //Console.WriteLine(currentEventIdentifier);  
+                //Show a messagebox displaying the contents of the string: currentEventIdentifier
+                MessageBox.Show(currentEventIdentifier);
+                
+            }  
         }
     }
 }
